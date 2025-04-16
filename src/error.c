c4ff9cf78421d1badd94d2d0533e17c9365d6d2f
erico-ke <eliasricokergaravat@gmail.com>
2025-04-16 15:10:16 +0200

    started  main and error control

--- src/error.c
+++ src/error.c
@@ -1,27 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pipex.h                                            :+:      :+:    :+:   */
+/*   error.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/10 16:37:43 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/04/16 15:06:01 by erico-ke         ###   ########.fr       */
+/*   Created: 2025/04/16 14:47:14 by erico-ke          #+#    #+#             */
+/*   Updated: 2025/04/16 15:06:30 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PIPEX_H
-# define PIPEX_H
-# include "../libs/libft/src/libft.h"
+#include "../includes/pipex.h"
 
-typedef struct pipex
+int	print_error(char *str)
 {
-	char	*filename1;
-	char	*filename2;
-	char	*cmd1;
-	char	*cmd2;
-}	t_pip;
-
-int	print_error(char *str);
-
-#endif
+	ft_printf("Error, %s.\n", 2, str);
+	return (EXIT_FAILURE);
+}
--- src/main.c
+++ src/main.c
@@ -6,13 +6,31 @@
 /*   By: erico-ke <erico-ke@42malaga.student.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/10 15:13:38 by erico-ke          #+#    #+#             */
-/*   Updated: 2025/04/10 18:05:47 by erico-ke         ###   ########.fr       */
+/*   Updated: 2025/04/16 15:06:46 by erico-ke         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
+void	ft_init(char **argv, t_pip	*lst)
+{
+	lst->filename1 = argv[1];
+	lst->cmd1 = argv[2];
+	lst->filename2 = argv[3];
+	lst->cmd2 = argv[4];
+}
+
 int	main(int argc, char **argv)
 {
-	
+	if (argc == 5)
+	{
+		t_pip	*lst;
+		lst = ft_calloc(1, sizeof(t_pip));
+		if (!lst)
+			return (print_error("Memory alloc failed"));
+		ft_init(argv, lst);
+	}
+	else
+		return(print_error("Incorrect number of arguments"));
+	return (EXIT_SUCCESS);
 }
